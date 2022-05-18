aa300d7a6081fcf187c17e8826e6413e06c68bea
Sagnik <indian.sagnik@gmail.com>
2022-05-18 10:00:30 +0530

    PHP files

---
+++ src/PHP_files/dashboard.php
@@ -0,0 +1,13 @@
+<?php 
+require "loginCheck.php"
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashobaord</title>
+</head>
+<body>
+	Welcome <?php echo $_SESSION["email"];?>
+</body>
+</html>
---
+++ src/PHP_files/resetpass.php
@@ -0,0 +1,23 @@
+<?php 
+if (isset($_GET["email"]) && isset($_GET["token"])) {
+$connection= new mysqli("localhost", "root","", "test");
+    $email = $connection->real_escape_string($_GET["email"]);
+    $token = $connection->real_escape_string($_GET["token"]);
+    $data = $connection->query("SELECT id FROM user WHERE email='$email' AND token='$token'");
+ if($data->num_rows >0){
+             echo "please check your link";
+             $str="0123456789qwertyuioplkjhgfdsa";
+             $str = str_shuffle($str);
+             $str = substr($str, 0, 15); <?php
+ $password = sha1($str);
+             $connection->query("UPDATE user SET password='$password', token='' WHERE email='$email'");
+             echo "Your New Password is $str";
+         }else{ 
+ header('Location: index.php');
+             exit();
+         } 
+ }else{
+     header("Location: index.php");
+     exit();
+ } 
+ ?>
